23877896989c4f4ad47748b6a9c99fbd7c9174af
Michael Barnwell <m.p.barnwell@gmail.com>
2019-03-26 13:03:18 +0000

    Switching Tink to Bouncycastle for improved compatibility and merging core and signer modules (#2)
    Switching Tink to Bouncycastle for improved compatibility and merging core and signer modules

--- doordeck-client/src/main/java/com/doordeck/sdk/jwt/Claims.java
+++ doordeck-client/src/main/java/com/doordeck/sdk/jwt/Claims.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
-import org.joda.time.DateTimeUtils;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
 
