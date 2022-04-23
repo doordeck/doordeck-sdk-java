4120d477418aab6a56990f7555ed81bb27d1ecb4
Rafa <raruizmu@gmail.com>
2022-04-24 00:57:29 +0200

    Secure storage library as jar

--- ui/src/main/java/com/doordeck/sdk/common/manager/Datastore.kt
+++ ui/src/main/java/com/doordeck/sdk/common/manager/Datastore.kt
@@ -1,5 +1,6 @@
 package com.doordeck.sdk.common.manager
 
+import android.content.Context
 import com.doordeck.sdk.common.utils.SecurePreferencesHelper
 import com.doordeck.sdk.dto.certificate.CertificateChain
 import com.doordeck.sdk.jackson.Jackson
@@ -12,17 +13,17 @@ import java.security.PublicKey
 internal class Datastore {
 
     // store pub/priv key in the safe android keychain
-    fun saveKeyPair(keyPair: KeyPair) {
+    fun saveKeyPair(context: Context, keyPair: KeyPair) {
         val om = Jackson.sharedObjectMapper()
-        SecurePreferences.setValue(PUB_KEY, om.writeValueAsString(keyPair.public))
-        SecurePreferences.setValue(PRIV_KEY, om.writeValueAsString(keyPair.private))
+        SecurePreferences.setValue(context, PUB_KEY, om.writeValueAsString(keyPair.public))
+        SecurePreferences.setValue(context, PRIV_KEY, om.writeValueAsString(keyPair.private))
     }
 
 
     // retrieve pub/priv key in the safe android keychain
-    fun getKeyPair(): KeyPair? {
-        val pubKeyStr = SecurePreferences.getStringValue(PUB_KEY, null)
-        val privKeyStr = SecurePreferences.getStringValue(PRIV_KEY, null)
+    fun getKeyPair(context: Context): KeyPair? {
+        val pubKeyStr = SecurePreferences.getStringValue(context, PUB_KEY, null)
+        val privKeyStr = SecurePreferences.getStringValue(context, PRIV_KEY, null)
         if (pubKeyStr == null || privKeyStr == null)
             return null
         val om = Jackson.sharedObjectMapper()
@@ -48,14 +49,14 @@ internal class Datastore {
     }
 
     // store certificates in the safe android keychain
-    fun saveAuthToken (authToken: String) {
-        SecurePreferencesHelper.setLongStringValue(TOKEN, authToken)
+    fun saveAuthToken (context: Context, authToken: String) {
+        SecurePreferencesHelper.setLongStringValue(context, TOKEN, authToken)
     }
 
 
     // retrieve certificates in the safe android keychain
-    fun getAuthToken(): String? {
-        return SecurePreferencesHelper.getLongStringValue(TOKEN)
+    fun getAuthToken(context: Context): String? {
+        return SecurePreferencesHelper.getLongStringValue(context, TOKEN)
     }
 
     // store certificates in the safe android keychain
@@ -74,8 +75,8 @@ internal class Datastore {
     }
 
 
-    public fun clean() {
-        SecurePreferences.clearAllValues()
+    public fun clean(context: Context) {
+        SecurePreferences.clearAllValues(context)
         Doordeck.sharedPreference?.clearSharedPreference()
     }
 
