c146537558a4354c63dab696cd58c706a80d907e
Michael Barnwell <michael.barnwell@sentryinteractive.com>
2026-06-19 13:57:00 +0100

    Bumping vulnerable dependencies and adding deprecation notice (#63)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,23 @@ buildscript {
             force(libs.jose4j)
 
             eachDependency {
-                if (requested.group == "io.netty") {
-                    useVersion(libs.versions.netty.get())
-                    because("Various security fixes")
+                when {
+                    requested.group == "io.netty" -> {
+                        useVersion(libs.versions.netty.get())
+                        because("Various security fixes")
+                    }
+                    requested.group == "org.bouncycastle" -> {
+                        useVersion(libs.versions.bouncycastle.get())
+                        because("Timing channel (CVE-2026-5598), LDAP injection and broken-crypto fixes")
+                    }
+                    requested.group == "org.apache.commons" && requested.name == "commons-lang3" -> {
+                        useVersion(libs.versions.commonsLang3.get())
+                        because("Uncontrolled recursion fix (CVE-2025-48924)")
+                    }
+                    requested.group == "org.apache.httpcomponents" && requested.name == "httpclient" -> {
+                        useVersion(libs.versions.httpclient.get())
+                        because("Cross-site scripting fix (CVE-2020-13956)")
+                    }
                 }
             }
         }
@@ -22,9 +36,19 @@ buildscript {
 allprojects {
     configurations.all {
         resolutionStrategy.eachDependency {
-            if (requested.group == "io.netty") {
-                useVersion(rootProject.libs.versions.netty.get())
-                because("Various security fixes")
+            when {
+                requested.group == "io.netty" -> {
+                    useVersion(rootProject.libs.versions.netty.get())
+                }
+                requested.group == "org.bouncycastle" -> {
+                    useVersion(rootProject.libs.versions.bouncycastle.get())
+                }
+                requested.group == "org.apache.commons" && requested.name == "commons-lang3" -> {
+                    useVersion(rootProject.libs.versions.commonsLang3.get())
+                }
+                requested.group == "org.apache.httpcomponents" && requested.name == "httpclient" -> {
+                    useVersion(rootProject.libs.versions.httpclient.get())
+                }
             }
         }
     }
